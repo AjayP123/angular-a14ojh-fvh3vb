ba6a9e9c3e78ecc2763994931152a0f5f4c17fc5
AjayP123 <ajay.polampalli1@gmail.com>
2022-07-20 17:40:23 +0100

    8 files modified, 2 added, 2 deleted

--- src/app/zippy-multislot/zippy-multislot.component.ts
+++ src/app/zippy-multislot/zippy-multislot.component.ts
@@ -6,7 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./zippy-multislot.component.scss'],
 })
 export class ZippyMultislotComponent {
-  public readonly items: Array<number> = [1, 2];
+  public readonly items: Array<number> = [1];
+  public readonly: Array<{ header: string; templateType: number }> = [
+    { header: 'Start Date-End Date', templateType: 2 },
+    { header: 'Start Time-End Time', templateType: 2 },
+    { header: 'Battery Start-Battery End', templateType: 2 },
+    { header: 'Total Charging Duration', templateType: 1 },
+    { header: 'Charging Location', templateType: 1 },
+  ];
+  displayedColumns = [
+    'auditEventType',
+    'serialNumber',
+    'createdBy',
+    'responsive',
+  ];
+  public auditLogs = [
+    { auditEventType: '06/07/2022', serialNumber: 1, createdBy: 'test_a' },
+    { auditEventType: 'test', serialNumber: 1, createdBy: 'test_a' },
+    { auditEventType: 'test', serialNumber: 1, createdBy: 'test_a' },
+    { auditEventType: 'test', serialNumber: 1, createdBy: 'test_a' },
+  ];
 }
 
 /*
