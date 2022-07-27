3cef5715ba794aff4f902ec9d71efe215c5bace9
AjayP123 <ajay.polampalli1@gmail.com>
2022-07-27 12:44:40 +0100

    2 files modified, 1 added

--- src/app/zippy-multislot/zippy-multislot.component.ts
+++ src/app/zippy-multislot/zippy-multislot.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { dataTable } from './data.table.constant';
 @Component({
   selector: 'app-zippy-multislot',
   templateUrl: './zippy-multislot.component.html',
   styleUrls: ['./zippy-multislot.component.scss'],
 })
 export class ZippyMultislotComponent {
+  @Input() dataSource: any = dataTable.ReportContent;
   public readonly items: Array<number> = [1];
   public readonly: Array<{ header: string; templateType: number }> = [
     { header: 'Start Date-End Date', templateType: 2 },
@@ -14,6 +15,16 @@ export class ZippyMultislotComponent {
     { header: 'Total Charging Duration', templateType: 1 },
     { header: 'Charging Location', templateType: 1 },
   ];
+  @Input() columsHeading: any = [
+    'Start Date',
+    'Start Time',
+    'Battery Start',
+    'Total Charging Duration',
+    'Charging Location',
+    'Distance Since Last Charge',
+    'Assigned Driver',
+    'responsive',
+  ];
   displayedColumns = [
     'auditEventType',
     'serialNumber',
@@ -21,6 +32,13 @@ export class ZippyMultislotComponent {
     'responsive',
   ];
   totalColumns = ['total'];
+  getTotal(index: number, totalParameter: string, element?: any) {
+    console.log(element);
+    return this.dataSource.Sections[index].Footer.filter(
+      (f: any) => f.Type === totalParameter
+    )[0].Value;
+    return 2;
+  }
   public auditLogs = [
     { auditEventType: '06/07/2022', serialNumber: 1, createdBy: 'test_a' },
     { auditEventType: 'test', serialNumber: 1, createdBy: 'test_a' },
