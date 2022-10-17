93475e60d4d3dc06d6ea2d65546070b7fe2fe059
AjayP123 <30439725+AjayP123@users.noreply.github.com>
2022-10-17 16:19:14 +0100

    8 files modified

--- src/app/checkboxlist/checkbox-list.component.ts
+++ src/app/checkboxlist/checkbox-list.component.ts
@@ -15,5 +15,17 @@ export class CheckboxListComponent {
     { label: 'Vehcile 2' },
     { label: 'Vehcile 3' },
     { label: 'Vehcile 4' },
+    { label: 'Vehcile 5' },
+    { label: 'Vehcile 6' },
+    { label: 'Vehcile 7' },
+    { label: 'Vehcile 8' },
+    { label: 'Vehcile 9' },
+    { label: 'Vehcile 10' },
+    { label: 'Vehcile 11' },
+    { label: 'Vehcile 12' },
+    { label: 'Vehcile 13' },
+    { label: 'Vehcile 14' },
+    { label: 'Vehcile 15' },
+    { label: 'Vehcile 16' },
   ];
 }
